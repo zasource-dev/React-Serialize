dd85ac7c958bd95526a041b387534d7823074137
pravdomil <info@pravdomil.com>
2018-04-20 15:51:47 +0200

    example

--- example/index.js
+++ example/index.js
@@ -1,7 +1,13 @@
 import React from "react"
 import { serialize, deserialize } from "react-serialize"
 
-const input = <p>Hello</p>
+const input = (
+  <div>
+    <h1 align="center">
+      <blink>Hello</blink>
+    </h1>
+  </div>
+)
 console.log("input", input)
 
 const json = serialize(input)
